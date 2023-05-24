e9ac827d316058e87a36d8af86955cc03f785204
Tunisia Artope <tartope@xpanxion.com>
2023-05-23 20:26:40 -0400

    Add square with Hello World.

---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Square.css'
+
+export const Square = () => {
+  return (
+    <div className='square'>
+        <p>
+            Hello, World!
+        </p>
+    </div>
+  )
+}
